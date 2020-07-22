1163c687fe66c73c127bdaacc18e89f034c04c9a
TwilightGalaxy <andyzhenghan@gmail.com>
2020-07-21 19:15:26 -0700

    footer, nav, img resize
    see above

---
+++ footer.js
@@ -0,0 +1,9 @@
+document.write(
+    '<footer>'+
+        '<div class="row">'+
+            '<div class="col-lg-12">'+
+                '<p>Copyright &copy; UWROV 2015-2020</p>'+
+            '</div>'+
+        '</div>'+
+    '</footer>'
+)
