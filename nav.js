2a23356c7bf9c3fc789124cc3e7a3d76f8c2a823
TwilightGalaxy <andyzhenghan@gmail.com>
2020-09-01 17:33:42 -0700

    Update logo, change title

--- nav.js
+++ nav.js
@@ -9,7 +9,7 @@ document.write(
             '<span class="icon-bar"></span>'+
             '<span class="icon-bar"></span>'+
         '</button>'+
-        '<a class="navbar-brand" href="index.html"><span style="color:#EAD4A3">UW</span><span style="color:#887D9D">ROV</span></a>'+
+        '<a class="navbar-brand" href="index.html"><span><img class="logo" src="./images/uwrov-logo.png"></span></a>'+
     '</div>'+
     '<!-- Collect the nav links, forms, and other content for toggling -->'+
     '<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">'+
